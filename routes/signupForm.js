da9ded4cd0e9b198d8f9124be929468ac7e82e39
limbo <benz16082005@gmail.com>
2023-04-19 12:44:05 +0600

    Replaced Files

--- routes/signupForm.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// get to the signup form
-router.get("/", (req, res) => {
-  res.render("signup");
-});
-
-module.exports = router;
